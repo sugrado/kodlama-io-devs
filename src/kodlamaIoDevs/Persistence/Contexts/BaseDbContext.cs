7f2ecd0847705a3875aeba7c76646ea952e0a373
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-09-01 18:47:22 +0300

    created database context

---
+++ src/kodlamaIoDevs/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+    }
+}
