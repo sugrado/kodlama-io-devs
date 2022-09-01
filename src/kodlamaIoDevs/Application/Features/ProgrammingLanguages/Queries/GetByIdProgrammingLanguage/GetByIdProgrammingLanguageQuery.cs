f37766de916f53108beca31866e5d9d20b86ea49
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-09-01 18:51:04 +0300

    get all programming languages and get by id programming language queries

---
+++ src/kodlamaIoDevs/Application/Features/ProgrammingLanguages/Queries/GetByIdProgrammingLanguage/GetByIdProgrammingLanguageQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage
+{
+    public class GetByIdProgrammingLanguageQuery : IRequest<ProgrammingLanguageGetByIdDto>
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetByIdProgrammingLanguageQueryHandler : IRequestHandler<GetByIdProgrammingLanguageQuery, ProgrammingLanguageGetByIdDto>
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+        private readonly IMapper _mapper;
+        private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+        public GetByIdProgrammingLanguageQueryHandler(
+            IProgrammingLanguageRepository programmingLanguageRepository,
+            IMapper mapper,
+            ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+            _mapper = mapper;
+            _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+        }
+        public async Task<ProgrammingLanguageGetByIdDto> Handle(GetByIdProgrammingLanguageQuery request, CancellationToken cancellationToken)
+        {
+            var programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == request.Id);
+            _programmingLanguageBusinessRules.ProgrammingLanguageShouldExistWhenRequested(programmingLanguage);
+            return _mapper.Map<ProgrammingLanguageGetByIdDto>(programmingLanguage);
+        }
+    }
+}
