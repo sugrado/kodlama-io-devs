bb5903a4158e78b382830d63655d7ba8d9f5ea6b
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-09-01 18:49:07 +0300

    created domain and infrastructure layer and create programming language command

---
+++ src/kodlamaIoDevs/Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCanNotBeDuplicatedWhenRequested(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name.ToLower() == name.ToLower());
+            if (result.Items.Any()) throw new BusinessException("The Programming Language name is already exists.");
+        }
+
+        public void ProgrammingLanguageShouldExistWhenRequested(ProgrammingLanguage? programmingLanguage)
+        {
+            if (programmingLanguage == null) throw new BusinessException("Requested Programming Language does not exist.");
+        }
+    }
+}
