d4a5f5447561344af5a2e17d3517392cfa95dcdb
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:46:50 +0300

    command operations

---
+++ src/kodlamaIoDevs/Application/Features/OperationClaims/Commands/CreateOperationClaim/CreateOperationClaimCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.Features.OperationClaims.Commands.CreateOperationClaim
+{
+    public class CreateOperationClaimCommandValidator : AbstractValidator<CreateOperationClaimCommand>
+    {
+        public CreateOperationClaimCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotNull()
+                .NotEmpty()
+                    .WithMessage("The Operation Claim name is required.")
+                .MinimumLength(3)
+                    .WithMessage("Operation claim name must be at least 3 characters long.");
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/Application/Features/OperationClaims/Commands/UpdateOperationClaim/UpdateOperationClaimCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.OperationClaims.Dtos;
+using Application.Features.OperationClaims.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.OperationClaims.Commands.UpdateOperationClaim
+{
+    public class UpdateOperationClaimCommand : IRequest<UpdatedOperationClaimDto>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UpdateOperationClaimCommandHandler : IRequestHandler<UpdateOperationClaimCommand, UpdatedOperationClaimDto>
+    {
+        private readonly IOperationClaimRepository _operationClaimRepository;
+        private readonly OperationClaimBusinessRules _operationClaimBusinessRules;
+        private readonly IMapper _mapper;
+
+        public UpdateOperationClaimCommandHandler(
+            IOperationClaimRepository operationClaimRepository,
+            OperationClaimBusinessRules operationClaimBusinessRules,
+            IMapper mapper)
+        {
+            _operationClaimRepository = operationClaimRepository;
+            _operationClaimBusinessRules = operationClaimBusinessRules;
+            _mapper = mapper;
+        }
+
+        public async Task<UpdatedOperationClaimDto> Handle(UpdateOperationClaimCommand request, CancellationToken cancellationToken)
+        {
+            var operationClaim = await _operationClaimRepository.GetAsync(p => p.Id == request.Id);
+            _operationClaimBusinessRules.OperationClaimShouldExistWhenRequested(operationClaim);
+            await _operationClaimBusinessRules.OperationClaimNameCanNotBeDuplicatedWhenRequested(request.Name);
+
+            _mapper.Map(request, operationClaim);
+            var updatedOperationClaim = await _operationClaimRepository.UpdateAsync(operationClaim);
+            return _mapper.Map<UpdatedOperationClaimDto>(updatedOperationClaim);
+        }
+    }
+}
