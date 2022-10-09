091fce684653937a78c8fa5104bac998754e52ab
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:52:09 +0300

    command operations

---
+++ src/kodlamaIoDevs/Application/Features/UserOperationClaims/Commands/CreateUserOperationClaim/CreateUserOperationClaimCommand.cs
@@ -0,0 +1,52 @@
+﻿using Application.Features.OperationClaims.Rules;
+using Application.Features.UserOperationClaims.Dtos;
+using Application.Features.UserOperationClaims.Rules;
+using Application.Features.Users.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Security.Entities;
+using MediatR;
+
+namespace Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim
+{
+    public class CreateUserOperationClaimCommand : IRequest<CreatedUserOperationClaimDto>
+    {
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+
+    public class CreateUserOperationClaimCommandHandler : IRequestHandler<CreateUserOperationClaimCommand, CreatedUserOperationClaimDto>
+    {
+        private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+        private readonly IMapper _mapper;
+        private readonly UserOperationClaimBusinessRules _userOperationClaimBusinessRules;
+        private readonly OperationClaimBusinessRules _operationClaimBusinessRules;
+        private readonly UserBusinessRules _userBusinessRules;
+
+        public CreateUserOperationClaimCommandHandler(
+            IUserOperationClaimRepository userOperationClaimRepository,
+            IMapper mapper,
+            UserOperationClaimBusinessRules userOperationClaimBusinessRules,
+            OperationClaimBusinessRules operationClaimBusinessRules,
+            UserBusinessRules userBusinessRules)
+        {
+            _userOperationClaimRepository = userOperationClaimRepository;
+            _mapper = mapper;
+            _userOperationClaimBusinessRules = userOperationClaimBusinessRules;
+            _operationClaimBusinessRules = operationClaimBusinessRules;
+            _userBusinessRules = userBusinessRules;
+        }
+
+        public async Task<CreatedUserOperationClaimDto> Handle(CreateUserOperationClaimCommand request, CancellationToken cancellationToken)
+        {
+            await _operationClaimBusinessRules.OperationClaimShouldExistWhenRequested(request.OperationClaimId);
+            await _userBusinessRules.UserShouldExistWhenRequested(request.UserId);
+            await _userOperationClaimBusinessRules.UserOperationClaimCanNotBeDuplicatedWhenRequested(request.UserId, request.OperationClaimId);
+
+            UserOperationClaim mappedUserOperationClaim = _mapper.Map<UserOperationClaim>(request);
+            UserOperationClaim createdUserOperationClaim = await _userOperationClaimRepository.AddAsync(mappedUserOperationClaim);
+            CreatedUserOperationClaimDto createdUserOperationClaimDto = _mapper.Map<CreatedUserOperationClaimDto>(createdUserOperationClaim);
+            return createdUserOperationClaimDto;
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/Application/Features/UserOperationClaims/Commands/UpdateUserOperationClaim/UpdateUserOperationClaimCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim
+{
+    public class UpdateUserOperationClaimCommandValidator : AbstractValidator<UpdateUserOperationClaimCommand>
+    {
+        public UpdateUserOperationClaimCommandValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(p => p.OperationClaimId)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
