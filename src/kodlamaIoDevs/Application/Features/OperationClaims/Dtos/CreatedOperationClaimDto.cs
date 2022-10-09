47953c0bdbe461f82bccc41ae5540a30a49747e8
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:46:41 +0300

    dtos, models, mapping profiles and business rules

---
+++ src/kodlamaIoDevs/Application/Features/OperationClaims/Dtos/CreatedOperationClaimDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Features.OperationClaims.Dtos
+{
+    public class CreatedOperationClaimDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/kodlamaIoDevs/Application/Features/OperationClaims/Rules/OperationClaimBusinessRules.cs
@@ -0,0 +1,38 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.OperationClaims.Rules
+{
+    public class OperationClaimBusinessRules
+    {
+        private readonly IOperationClaimRepository _operationClaimRepository;
+
+        public OperationClaimBusinessRules(IOperationClaimRepository operationClaimRepository)
+        {
+            _operationClaimRepository = operationClaimRepository;
+        }
+
+        public async Task OperationClaimNameCanNotBeDuplicatedWhenRequested(string name)
+        {
+            bool result = await _operationClaimRepository
+                .Query()
+                .Where(p => p.Name.ToLower() == name.ToLower())
+                .AnyAsync();
+
+            if (result) throw new BusinessException("The Operation Claim name is already exists.");
+        }
+
+        public void OperationClaimShouldExistWhenRequested(OperationClaim? operationClaim)
+        {
+            if (operationClaim == null) throw new BusinessException("Requested Operation Claim does not exist.");
+        }
+
+        public async Task OperationClaimShouldExistWhenRequested(int id)
+        {
+            var operationClaim = await _operationClaimRepository.GetAsync(p => p.Id == id);
+            if (operationClaim is null) throw new BusinessException("Requested Operation Claim does not exist.");
+        }
+    }
+}
