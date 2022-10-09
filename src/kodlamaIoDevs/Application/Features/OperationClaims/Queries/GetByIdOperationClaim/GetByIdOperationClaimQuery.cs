86b45e2f36dbed59566cd74232da6f967c672b58
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:46:58 +0300

    query operations

---
+++ src/kodlamaIoDevs/Application/Features/OperationClaims/Queries/GetByIdOperationClaim/GetByIdOperationClaimQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.OperationClaims.Dtos;
+using Application.Features.OperationClaims.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.OperationClaims.Queries.GetByIdOperationClaim
+{
+    public class GetByIdOperationClaimQuery : IRequest<OperationClaimGetByIdDto>
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetByIdOperationClaimQueryHandler : IRequestHandler<GetByIdOperationClaimQuery, OperationClaimGetByIdDto>
+    {
+        private readonly IOperationClaimRepository _operationClaimRepository;
+        private readonly IMapper _mapper;
+        private readonly OperationClaimBusinessRules _operationClaimBusinessRules;
+        public GetByIdOperationClaimQueryHandler(
+            IOperationClaimRepository operationClaimRepository,
+            IMapper mapper,
+            OperationClaimBusinessRules operationClaimBusinessRules)
+        {
+            _operationClaimRepository = operationClaimRepository;
+            _mapper = mapper;
+            _operationClaimBusinessRules = operationClaimBusinessRules;
+        }
+        public async Task<OperationClaimGetByIdDto> Handle(GetByIdOperationClaimQuery request, CancellationToken cancellationToken)
+        {
+            var OperationClaim = await _operationClaimRepository.GetAsync(p => p.Id == request.Id);
+            _operationClaimBusinessRules.OperationClaimShouldExistWhenRequested(OperationClaim);
+            return _mapper.Map<OperationClaimGetByIdDto>(OperationClaim);
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/Application/Features/OperationClaims/Queries/GetListOperationClaim/GetListOperationClaimQuery.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.OperationClaims.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using MediatR;
+
+namespace Application.Features.OperationClaims.Queries.GetListOperationClaim
+{
+    public class GetListOperationClaimQuery : IRequest<OperationClaimListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+    }
+
+    public class GetListOperationClaimQueryHandler : IRequestHandler<GetListOperationClaimQuery, OperationClaimListModel>
+    {
+        private readonly IOperationClaimRepository _operationClaimRepository;
+        private readonly IMapper _mapper;
+
+        public GetListOperationClaimQueryHandler(IOperationClaimRepository operationClaimRepository, IMapper mapper)
+        {
+            _operationClaimRepository = operationClaimRepository;
+            _mapper = mapper;
+        }
+        public async Task<OperationClaimListModel> Handle(GetListOperationClaimQuery request, CancellationToken cancellationToken)
+        {
+            var OperationClaims = await _operationClaimRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+            return _mapper.Map<OperationClaimListModel>(OperationClaims);
+        }
+    }
+}
