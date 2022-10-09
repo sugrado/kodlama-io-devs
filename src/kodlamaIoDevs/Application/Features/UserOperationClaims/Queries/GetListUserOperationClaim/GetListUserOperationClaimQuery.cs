2a2c2f7249934eb4a31e29fd7b94cadc6eceec0d
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:52:45 +0300

    queries operations and controller

---
+++ src/kodlamaIoDevs/Application/Features/UserOperationClaims/Queries/GetListUserOperationClaim/GetListUserOperationClaimQuery.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.UserOperationClaims.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using MediatR;
+
+namespace Application.Features.UserOperationClaims.Queries.GetListUserOperationClaim
+{
+    public class GetListUserOperationClaimQuery : IRequest<UserOperationClaimListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+    }
+
+    public class GetListUserOperationClaimQueryHandler : IRequestHandler<GetListUserOperationClaimQuery, UserOperationClaimListModel>
+    {
+        private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+        private readonly IMapper _mapper;
+
+        public GetListUserOperationClaimQueryHandler(IUserOperationClaimRepository userOperationClaimRepository, IMapper mapper)
+        {
+            _userOperationClaimRepository = userOperationClaimRepository;
+            _mapper = mapper;
+        }
+        public async Task<UserOperationClaimListModel> Handle(GetListUserOperationClaimQuery request, CancellationToken cancellationToken)
+        {
+            var userOperationClaims = await _userOperationClaimRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+            return _mapper.Map<UserOperationClaimListModel>(userOperationClaims);
+        }
+    }
+}
