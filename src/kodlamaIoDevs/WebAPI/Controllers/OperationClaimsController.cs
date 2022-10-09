22507a474a30aa94021b6b662ed7fefd305d69e8
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:48:33 +0300

    Operation claim crud (#3)
    * dtos, models, mapping profiles and business rules
    
    * command operations
    
    * query operations
    
    * controller and persistence repository

---
+++ src/kodlamaIoDevs/WebAPI/Controllers/OperationClaimsController.cs
@@ -0,0 +1,51 @@
+﻿using Application.Features.OperationClaims.Commands.CreateOperationClaim;
+using Application.Features.OperationClaims.Commands.DeleteOperationClaim;
+using Application.Features.OperationClaims.Commands.UpdateOperationClaim;
+using Application.Features.OperationClaims.Queries.GetByIdOperationClaim;
+using Application.Features.OperationClaims.Queries.GetListOperationClaim;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationClaimsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateOperationClaimCommand createOperationClaimCommand)
+        {
+            var result = await Mediator.Send(createOperationClaimCommand);
+            return Created("", result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateOperationClaimCommand updateOperationClaimCommand)
+        {
+            var result = await Mediator.Send(updateOperationClaimCommand);
+            return Ok(result);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete([FromRoute] DeleteOperationClaimCommand deleteOperationClaimCommand)
+        {
+            var result = await Mediator.Send(deleteOperationClaimCommand);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var getListOperationClaimQuery = new GetListOperationClaimQuery { PageRequest = pageRequest };
+            var result = await Mediator.Send(getListOperationClaimQuery);
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdOperationClaimQuery getByIdOperationClaimQuery)
+        {
+            var operationClaimGetByIdDto = await Mediator.Send(getByIdOperationClaimQuery);
+            return Ok(operationClaimGetByIdDto);
+        }
+    }
+}
