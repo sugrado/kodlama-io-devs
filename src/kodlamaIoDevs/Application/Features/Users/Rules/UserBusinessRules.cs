e8a28d45798fd1faa22ac250ebd35adb0797f4af
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-10-09 13:56:25 +0300

    user business rules and refresh token repository

---
+++ src/kodlamaIoDevs/Application/Features/Users/Rules/UserBusinessRules.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Security.Entities;
+
+namespace Application.Features.Users.Rules
+{
+    public class UserBusinessRules
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserBusinessRules(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task UserShouldExistWhenRequested(int userId)
+        {
+            var userFound = await _userRepository.GetAsync(p => p.Id == userId);
+            if (userFound is null)
+                throw new BusinessException("User not found.");
+        }
+
+        public void UserShouldExistWhenRequested(User? userFound)
+        {
+            if (userFound is null)
+                throw new BusinessException("User not found.");
+        }
+    }
+}
